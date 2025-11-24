0cc25fe93ece0e8180b345c5ac1475b270c9876a
Daniel Borre <danisecundarioxd@gmail.com>
2025-11-24 16:52:47 -0500

    cambie comentario

--- init.lua
+++ init.lua
@@ -28,10 +28,6 @@ What is Kickstart?
     The goal is that you can read every line of code, top-to-bottom, understand
     what your configuration is doing, and modify it to suit your needs.
 
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
     If you don't know anything about Lua, I recommend taking some time to read through
     a guide. One possible example which will only take 10-15 minutes:
       - https://learnxinyminutes.com/docs/lua/
